066dec4da9230bfd3cc6692a3f39cdeba883078f
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-09-25 14:07:52 +0100

    Cloned.

---
+++ backup/moodle2/backup_sandbox_stepslib.php
@@ -0,0 +1,74 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Define all the backup steps that will be used by the backup_sandbox_activity_task
+ *
+ * @package   mod_sandbox
+ * @category  backup
+ * @copyright 2019 Richard Jones richardnz@outlook.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Define the complete sandbox structure for backup, with file and id annotations.
+ *
+ * @package   mod_sandbox
+ * @category  backup
+ * @copyright 2019 Richard Jones richardnz@outlook.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @see https://github.com/moodlehq/moodle-mod_simplemod
+ * @see https://github.com/justinhunt/moodle-mod_simplemod
+ */
+class backup_sandbox_activity_structure_step extends backup_activity_structure_step {
+
+    /**
+     * Defines the backup structure of the module
+     *
+     * @return backup_nested_element
+     */
+    protected function define_structure() {
+
+        // Get know if we are including userinfo.
+        $userinfo = $this->get_setting_value('userinfo');
+
+        // Define the root element describing the sandbox instance.
+        $sandbox = new backup_nested_element('sandbox',
+            array('id'), 
+            array(
+                'course', 'name', 'intro',
+                'introformat', 'title', 'timecreated',
+                'timemodified'
+            )
+        );
+
+        // If we had more elements, we would build the tree here.
+
+        // Define data sources.
+        $sandbox->set_source_table('sandbox', array('id' => backup::VAR_ACTIVITYID));
+
+        /* If we were referring to other tables, we would annotate the relation
+           with the element's annotate_ids() method. */
+
+        // Define file annotations (we do not use itemid in this example).
+        $sandbox->annotate_files('mod_sandbox', 'intro', null);
+
+        // Return the root element (sandbox), wrapped into standard activity structure.
+        return $this->prepare_activity_structure($sandbox);
+    }
+}
---
+++ version.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Defines the version and other meta-info about the plugin
+ *
+ * @package    mod_sandbox
+ * @copyright  2019 Richard Jones richardnz@outlook.com
+ * @copyright  2021 G J Barnard - {@link http://moodle.org/user/profile.php?id=442195}.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @see https://github.com/moodlehq/moodle-mod_simplemod
+ * @see https://github.com/justinhunt/moodle-mod_simplemod
+ * @see https://github.com/richardjonesnz/moodle-mod_simplemod
+ * @see https://github.com/gjb2048/moodle-mod_simplemod
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'mod_sandbox';
+$plugin->version = 2021081400;
+$plugin->requires  = 2021051700.00; // 3.11 (Build: 20210517).
+$plugin->supported = array(311, 311);
+$plugin->release = 'V3.11.1.0';
+$plugin->maturity = MATURITY_STABLE;
